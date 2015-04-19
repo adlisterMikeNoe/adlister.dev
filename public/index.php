1d8a432414a2ac973cc74a5a1fe43ac4391df987
Noe Nevarez <closner_nevarez@hotmail.com>
2015-04-19 12:30:57 -0500

    added html and some css to files

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<html>
+<head>
+	<title>Most Interesting Ad Lister Ever</title>
+	<link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+	<?php require '../views/partials/navbar.php'; ?>
+
+
+
+
+
+
+
+	<?php require '../views/partials/footer.php'; ?>
+</body>
+</html>
