ff3e822c26117ece20ffdc11794bbeff65264287
Noe Nevarez <closner_nevarez@hotmail.com>
2015-04-20 15:57:03 -0500

    worked with the databse folder and models

---
+++ database/adlisting_login.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST','127.0.0.1');
+define('DB_NAME', 'adlisting_db');
+define('DB_USER', 'adlister_user');
+define('DB_PASS', 'password');
+
+?>
--- database/migration.php
+++ database/migration.php
@@ -1,2 +1,32 @@
+<?php
+require 'adlisting_login.php';
+require 'db_connect.php';
 
 
+$dbc->exec('DROP TABLE IF EXISTS users,ads');
+
+
+$query = 'CREATE TABLE users (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(100) NOT NULL, 
+	last_name VARCHAR(100) NOT NULL,
+	user_name VARCHAR(10) NOT NULL,
+	password VARCHAR(10) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	PRIMARY KEY (id)
+	)';
+$dbc->exec($query);
+
+
+$query = 'CREATE TABLE ads (
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100) NOT NULL,
+		price DOUBLE (10, 2) NOT NULL,
+		phone int(10) NOT NULL,
+		image BLOB,
+		description TEXT NOT NULL,
+		PRIMARY KEY (id)
+		)';
+$dbc->exec($query);
+
+?>
