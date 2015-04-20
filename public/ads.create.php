4ce6b47c8d4c28ba4bba8cfcf2b94785368ac7ca
Mike Villarreal <sales@moredesignonline.com>
2015-04-20 12:50:54 -0500

    Here is the page to create an ad

--- public/ads.create.php
+++ public/ads.create.php
@@ -3,6 +3,7 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -14,25 +15,30 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
+
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
+
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
+
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
+
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -42,23 +48,93 @@ if ($uploadOk == 0) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
 
-<DOCTYPE> <html>
-<head>
-	<title></title>
+    <title>Ad Lister | Create your ad</title>
+
+    <?php require "../views/partials/header.php"; ?>
+
+
 </head>
 <body>
 	<?php require '../views/partials/navbar.php'; ?>
-	<div class="myform">
-			<h3>Create an Ad</h3>
-			<form action="index.php" method="POST">
-			Price: <input type="text"  required name=" "><br>
-			Location: <input type="text" required name="location"><br>
-			Description: <input type="text" required name="description"><br>
-		    Select image to upload:
-		    <input type="file" name="fileToUpload" id="fileToUpload">
-			<input type="submit">
-			</form>
-</div>
+	<section class="container-fluid" id="section2">
+      
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- <div class="well well-sm"> -->
+                    <form class="form-horizontal" method="post">
+                        <fieldset>
+                            <legend class="text-center header">Create an Ad</legend>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-bars bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="title" name="title" type="text" placeholder="Title" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-usd bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="price" name="price" type="number" placeholder="Price" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-map-marker bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="location" name="location" type="text" placeholder="Location" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="phone" name="phone" type="tel" placeholder="Phone" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-cloud-upload bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <input id="fileToUpload" name="fileToUpload" type="file" class="form-control" multiple>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                                <div class="col-md-8">
+                                    <textarea class="form-control" id="description" name="descrition" placeholder="Enter a detail description for your product or service." rows="7"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                <!-- </div> -->
+            </div>
+        </div>
+        </div>
+
+</section>
+
+
+<?php require '../views/partials/footer.php'; ?>
+
+    
 </body>
 </html>
