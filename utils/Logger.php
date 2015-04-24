c78d103907b276cb6151ff42930a4d5ebd0cbb61
Mike Villarreal <sales@moredesignonline.com>
2015-04-24 13:29:58 -0500

    The right Files

--- utils/Logger.php
+++ utils/Logger.php
@@ -7,18 +7,15 @@
       public function __construct($prefix = 'log')
     {
         $this->filename = $prefix . date('Y-m-d');
-        $this->handle = fopen($this->filename, 'a');
+
+        $this->handle = fopen($this->filename, 'a+');
     }
 
  	public	function logMessage($logLevel, $message)
  		{
-<<<<<<< HEAD
-            $date = date('Y-m-d');
- 			fwrite($this->handle, $date . ' ' . $logLevel . $message . ' ' . time() . PHP_EOL);
-=======
+
             $date = date('l jS \of F Y h:i:s A');
  			fwrite($this->handle, $date . " " . $logLevel .  " " . $message . PHP_EOL);
->>>>>>> 883e176ddbd6d99b30c7c3ed751f0c15ca978c11
  			
         }
 
